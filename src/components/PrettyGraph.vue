9b6d23f81d9b090e28b37f0b01a82707eceefe30
k-okina <okinakahiro@gmail.com>
2018-09-03 15:51:49 +0900

    [delete] 余分なコードを掃除

---
+++ src/components/PrettyGraph.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    テスト
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class PrettyGraph extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <PrettyGraph msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import PrettyGraph from '@/components/PrettyGraph.vue'; // @ is an alias to /src
 
 @Component({
   components: {
-    HelloWorld,
+    PrettyGraph,
   },
 })
 export default class Home extends Vue {}

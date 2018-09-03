780db2a0c7333cbd55d2220679a7844f244f9ce0
k-okina <okinakahiro@gmail.com>
2018-09-03 19:31:14 +0900

    [add] d3js練習 3

--- src/components/PrettyGraph.vue
+++ src/components/PrettyGraph.vue
@@ -1,15 +1,87 @@
 <template>
-  <div>
-    テスト
-  </div>
+  <svg ref="targetSvg" :width="width" :height="height" style="background: gray"></svg>
 </template>
 
 <script lang="ts">
 import * as d3 from 'd3';
 import { Component, Prop, Vue } from 'vue-property-decorator';
 
+type Data = {
+  name: string;
+  score: number;
+}
+
 @Component
-export default class PrettyGraph extends Vue {
-  @Prop() private msg!: string;
+export default class AppCircle extends Vue {
+  @Prop({default: 600}) width!: number;
+  @Prop({default: 240}) height!: number;
+
+  mounted() {
+
+
+    const data = [
+      {name: 'taguchi', score: 717},
+      {name: 'fkoji', score: 920},
+      {name: 'tanaka', score: 120},
+      {name: 'takkun', score: 999},
+      {name: 'dotinstall', score: 618},
+    ];
+    const draw = (dataSet: Array<Data>) => {
+      const padding = {
+        top: 40,
+        bottom: 40,
+        left: 100,
+        right: 40,
+      };
+
+      const elm = this.$refs.targetSvg as Element;
+      const bars = d3.select(elm).selectAll('rect').data(dataSet);
+      const xScale = d3
+        .scaleLinear()
+        .domain([0, d3.max(dataSet, d => d.score) as Number])
+        .range([0, this.width - padding.left - padding.right])
+        .nice();
+
+      const yScale = d3
+        .scaleBand()
+        .domain(dataSet.map(data => data.name))
+        .range([0, this.height - padding.top - padding.bottom])
+        .padding(0.1);
+
+      bars
+        .enter()
+        .append('rect')
+        .merge(bars)
+        .attr('x', padding.left)
+        .attr('y', (d, i) => yScale(d.name) as number + padding.top)
+        .attr('width', d => xScale(d.score))
+        .attr('height', yScale.bandwidth())
+        .attr('fill', 'yellow')
+
+      d3
+        .select(elm)
+        .selectAll('.axis')
+        .remove()
+
+      d3
+        .select(elm)
+        .append('g')
+        .call(d3.axisBottom(xScale) as any)
+        .attr('class', 'axis')
+        .attr('transform', `translate(${padding.left}, ${this.height - padding.bottom})`);
+
+      d3
+        .select(elm)
+        .append('g')
+        .call(d3.axisLeft(yScale) as any)
+        .attr('class', 'axis')
+        .attr('transform', `translate(${padding.left}, ${padding.top})`);
+    }
+    draw(data);
+    draw([
+      {name: 'taguchi', score: 717},
+      {name: 'fkoji', score: 920},
+    ]);
+  };
 }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="home">
-    <!-- <PrettyGraph msg="Welcome to Your Vue.js + TypeScript App"/> -->
-    <AppCircle/>
+    <PrettyGraph/>
+    <!-- <AppCircle/> -->
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import AppCircle from '@/components/AppCircle.vue'; // @ is an alias to /src
-// import PrettyGraph from '@/components/PrettyGraph.vue'; // @ is an alias to /src
+// import AppCircle from '@/components/AppCircle.vue'; // @ is an alias to /src
+import PrettyGraph from '@/components/PrettyGraph.vue'; // @ is an alias to /src
 
 @Component({
   components: {
-    // PrettyGraph,
-    AppCircle,
+    PrettyGraph,
+    // AppCircle,
   },
 })
 export default class Home extends Vue {}
